f82801dae7b02eed6b0c90fb166372c664194520
Marco Guidara <marco.guidara@efesto.studio>
2021-05-18 00:42:26 +0200

    Fix Typo

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -5,19 +5,19 @@ function Expenses(props) {
   return (
     <div className="expenses">
       <ExpenseItem
-        title={props.expences[0].title}
-        amount={props.expences[0].amount}
-        date={props.expences[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={props.expences[1].title}
-        amount={props.expences[1].amount}
-        date={props.expences[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={props.expences[2].title}
-        amount={props.expences[2].amount}
-        date={props.expences[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
     </div>
   );
