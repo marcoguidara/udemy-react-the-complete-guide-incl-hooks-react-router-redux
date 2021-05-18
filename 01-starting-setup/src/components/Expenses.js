cf970540ea87f857e3ddc4c97e39ad4d4a32f631
Marco Guidara <marco.guidara@efesto.studio>
2021-05-18 22:39:17 +0200

    Lesson 39

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,9 +1,10 @@
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -19,7 +20,12 @@ function Expenses(props) {
         amount={props.items[2].amount}
         date={props.items[2].date}
       />
-    </div>
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
   );
 }
 
