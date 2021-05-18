f45b9c19f31cbe4ce9501fa9d2c0c450d13760b0
Marco Guidara <marco.guidara@efesto.studio>
2021-05-18 23:18:59 +0200

    Lesson 46

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => console.log('CLICKED');
 
   return (
     <Card className="expense-item">
@@ -12,6 +13,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
