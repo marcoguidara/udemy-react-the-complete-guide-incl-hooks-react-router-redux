508170ed58ef5290dc499276e871e6454aa70eec
Marco Guidara <marco.guidara@efesto.studio>
2021-06-28 08:40:17 +0200

    Lesson 78

--- 02-starting-project/src/components/UI/Button/Button.js
+++ 02-starting-project/src/components/UI/Button/Button.js
@@ -8,7 +8,11 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  width: 100%;
 
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
