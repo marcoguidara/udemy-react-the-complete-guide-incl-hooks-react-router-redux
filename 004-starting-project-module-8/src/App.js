87f3418bcd9a1bd4de48d9aa7fc6a110efb6482f
Marco Guidara <marco.guidara@efesto.studio>
2021-06-28 18:25:12 +0200

    Setup module 0. Lesson 89

---
+++ 004-starting-project-module-8/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './components/UI/Users/AddUser';
+
+function App() {
+  return (
+    <div>
+      <AddUser></AddUser>
+    </div>
+  );
+}
+
+export default App;
