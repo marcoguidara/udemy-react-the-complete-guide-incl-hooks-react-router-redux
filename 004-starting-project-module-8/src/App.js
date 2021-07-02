2981140f035e2add0663f56e4e6f4e2dbfea5e68
Marco Guidara <marco.guidara@efesto.studio>
2021-07-02 13:42:54 +0200

    Lesson 94

--- 004-starting-project-module-8/src/App.js
+++ 004-starting-project-module-8/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UsersList from './components/Users/UsersList';
 function App() {
   return (
     <div>
       <AddUser></AddUser>
+      <UsersList users={[]}></UsersList>
     </div>
   );
 }
