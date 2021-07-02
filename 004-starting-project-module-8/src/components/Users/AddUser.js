7cb56679e6f5b4cb6f8d553c52eaeb8baf7b9dfc
Marco Guidara <marco.guidara@efesto.studio>
2021-07-02 13:23:33 +0200

    Lesson 92

--- 004-starting-project-module-8/src/components/Users/AddUser.js
+++ 004-starting-project-module-8/src/components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername , enteredAge)
   }
+
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input  id="username" type="text"></input>
+        <input  id="username" type="text" onChange={usernameChangeHandler}></input>
         <label htmlFor="username">Age (Years)</label>
-        <input id="age" type="number"></input>
-        <Button type="button">Add User</Button>
+        <input id="age" type="number" onChange={ageChangeHandler}></input>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>    
   )
