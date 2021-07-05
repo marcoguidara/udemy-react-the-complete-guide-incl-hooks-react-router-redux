e6d1d431a8c487b5583c1753768b7c90f3f1e340
Marco Guidara <marco.guidara@efesto.studio>
2021-07-05 14:27:56 +0200

    Lesson 101

--- 004-starting-project-module-8/src/components/Users/AddUser.js
+++ 004-starting-project-module-8/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -42,7 +43,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error &&
       <ErrorModal
         title={error.title}
@@ -68,7 +69,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
