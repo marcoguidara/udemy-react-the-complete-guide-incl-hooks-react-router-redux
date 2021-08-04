93cae2a8fd7a890387020fff8c7b27706d90095c
Marco Guidara <marco.guidara@efesto.studio>
2021-08-04 08:52:10 +0200

    Lesson 142

--- 006-starting-project-modlue-11/src/components/Layout/HeaderCartButton.js
+++ 006-starting-project-modlue-11/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,24 @@
-import React from "react";
+import { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
-  return <button className={classes.button} onClick={props.onClick}>
-    <span className={classes.icon}>
-      <CartIcon />
-    </span>
-    <span>Yout Cart</span>
-    <span className={classes.badge}>
-      3
-    </span>
-  </button>;
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce(
+    (curNumber, item) => curNumber + item.amount,
+    0
+  );
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Yout Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
