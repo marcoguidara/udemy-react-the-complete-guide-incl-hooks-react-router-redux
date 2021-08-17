318e04b13b178451136e387d435eed4e711570e2
Marco Guidara <marco.guidara@efesto.studio>
2021-08-17 22:18:38 +0200

    Lesson 146

--- 006-starting-project-modlue-11/src/App.js
+++ 006-starting-project-modlue-11/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import CartProvider from "./store/CardProvider";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
