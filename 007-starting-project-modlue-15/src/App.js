f937467e1571fbe563b252dfbabc37dbec0c8077
Marco Guidara <marco.guidara@efesto.studio>
2021-09-20 14:40:55 +0200

    Lesson 190

--- 007-starting-project-modlue-15/src/App.js
+++ 007-starting-project-modlue-15/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttpRequest from './hooks/use-http-request';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-course-d6603-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
       );
 
       if (!response.ok) {
